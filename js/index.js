89198d86a6b127479722c2c576ce707e0a85a511
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-11-28 23:12:04 +0900

    랜덤번호 생성

--- js/index.js
+++ js/index.js
@@ -28,16 +28,32 @@ function draw() {
         });
     });
 }
-    
+
+function randomNumber() {
+    let blinkArr = [];
+    data.forEach(function(rowData, i) {
+        rowData.forEach(function(colData, j) {
+            if(!colData) {
+                blinkArr.push([i, j]);
+            }
+        });
+    });
+    console.log(blinkArr);
+    let randomBlock = blinkArr[Math.floor(Math.random() * blinkArr.length)];
+    data[randomBlock[0]][randomBlock[1]] = 2;
+    draw();
+}
 
 function main() {
 
     //게임 보드 생성
     createBorad();
-    draw();
+
+    //랜덤 생성
+    randomNumber();
 
     //각 방향으로 이동
-
+ 
     //이동된 숫자들 합쳐주는 함수
 
     //점수 계산
